3e15d41a6de5231f19aa6139a5711f331c7b18ff
Julian Rüth <julian.rueth@fsfe.org>
2021-08-30 01:36:05 +0200

    build on Windows with cmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,17 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
-if %ERRORLEVEL% neq 0 exit 1
+mkdir build
+cd build
+
+cmake ^
+  -G "Ninja" ^
+  -DBUILD_TESTING=ON ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DBUILD_SHARED_LIBS=ON ^
+  --debug-find ^
+  ..
+
+ninja -j%CPU_COUNT%
+ninja install
+
+ctest -j%CPU_COUNT% --output-on-failure -E "dump_file"
